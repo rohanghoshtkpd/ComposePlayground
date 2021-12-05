80914724b048c24d8e421d9317075d9f4a062491
rohanghosh <rohangho@live.com>
2021-12-06 01:11:57 +0530

    Added the basic frame work

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -4,8 +4,15 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposePlaygroundTheme {
                 // A surface container using the 'background' color from the theme
-               MyAppUI()
+                MyAppUI()
             }
         }
 
@@ -22,9 +29,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyAppUI()
-{
+fun MyAppUI() {
+    androidx.compose.material.Surface(modifier = Modifier.fillMaxWidth(), color = Color.LightGray) {
+        ProfileCard()
+    }
+}
 
+
+@Composable
+fun ProfileCard() {
+    Card(modifier = Modifier.fillMaxWidth(), elevation = 8.dp) {
+        (Row(modifier = Modifier.wrapContentSize()) {
+            ProfileCardImage()
+            ProfileContent()
+        })
+
+    }
+}
+
+@Composable
+fun ProfileContent() {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun ProfileCardImage() {
+    TODO("Not yet implemented")
 }
 
 
